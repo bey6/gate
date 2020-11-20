81a57f16219b3448b7d340174a27cce73ce3b4fa
Bei Liu <liub@docimax.com.cn>
2020-11-20 13:41:48 +0800

    feat: integrate with nedb.

--- routes/mapping.js
+++ routes/mapping.js
@@ -1,9 +1,36 @@
 const router = require('koa-router')()
+const db = require('../db/nedb')
 
 router.prefix('/mapping')
 
+router.post('/target', async (ctx, next) => {
+  try {
+    if (!ctx.request.body.origin) ctx.body = 'Origin 不可为空！'
+    else if (!ctx.request.body.action) ctx.body = 'Action 不可为空！'
+    else if (!ctx.request.body.version) ctx.body = 'Version 不可为空！'
+    else {
+      let res = await db.insert({
+        targetOrigin: ctx.request.body.origin,
+        targetAction: ctx.request.body.action,
+        targetVersion: ctx.request.body.version,
+      })
+      ctx.redirect('/mapping')
+    }
+  } catch (error) {
+    ctx.body = error
+  }
+})
+
 router.get('/', async (ctx, next) => {
-  await ctx.render('mapping')
+  try {
+    let res = await db.query()
+    await ctx.render('mapping', {
+      title: 'mapping',
+      list: res.map((r, idx) => ({ ...r, key: idx + 1 })),
+    })
+  } catch (error) {
+    ctx.body = error
+  }
 })
 
 module.exports = router
