02907656e7bf1149c4963f50184850dac1d5f118
Bei Liu <liub@docimax.com.cn>
2020-11-23 10:43:18 +0800

    feat: add log page

--- routes/log.js
+++ routes/log.js
@@ -3,13 +3,13 @@ const fs = require('fs')
 const showdown = require('showdown')
 const converter = new showdown.Converter()
 
-router.prefix('/document')
+router.prefix('/log')
 
 router.get('/', async (ctx, next) => {
-  let data = fs.readFileSync('articles/document.md')
+  let data = fs.readFileSync('articles/log.md')
   let html = converter.makeHtml(data.toString())
-  await ctx.render('document', {
-    title: 'document',
+  await ctx.render('log', {
+    title: 'log',
     article: html,
   })
 })
