ffcc40dea95692070e4371b048a544d5b24ac4e7
bey <bey.liu@outlook.com>
2020-11-23 23:48:48 +0800

    feat: add outline with document page and log page

--- articles/document.md
+++ articles/document.md
@@ -2,14 +2,32 @@
 
 `version: 1.0.0`
 
-## 尝试一下
+## Try
 
 - [获取权限系统部门](http://172.30.199.153:3000/auth/api/dictionary/departlist)
 - [获取质控系统质控任务](http://172.30.199.153:3000/mrqc/api/qualitytask/getqualitytasklist)
 - [获取病历管理系统 Scope](http://172.30.199.153:3000/mrms/api/consult/getemrmsscope)
 
-## 加入 Torii
+## Join Torii
 
 1. 进入 [Mapping](mapping) 页面
 2. 点击 `+ New` 按钮
 3. 输入 **Alias**、**Origin**、**Version** 后点击 `Save` 按钮即可
+
+## example
+
+### js
+
+```js
+function foo() {
+  return '1' + 2
+}
+```
+
+### go
+
+```go
+func foo(num int32) int32 {
+  return num * num
+}
+```
--- public/javascripts/document/index.js
+++ public/javascripts/document/index.js
@@ -1,8 +1,21 @@
-hljs.initHighlightingOnLoad()
-
-console.log('....')
-
 $(function () {
-  console.log('loaded')
-  console.log($('h1'))
+  hljs.initHighlightingOnLoad()
+
+  $('article>h2').each((idx, ele) => {
+    let li = $(
+      `<li><a href="#${ele.id}">${idx + 1}. ${ele.textContent}</a></li>`
+    )
+    $('#outline__root').append(li)
+    // console.log($(`article>#${ele.id}`))
+    // if ($(`article>#${ele.id}>h3`).length > 0) {
+    //   let ol = $('<ol></ol>')
+    //   $(`article>#${ele.id}>h3`).each((h3idx, h3ele) => {
+    //     let h3li = $(
+    //       `<li><a href="#${h3ele.id}">${idx + 1}. ${h3ele.textContent}</a></li>`
+    //     )
+    //     ol.append(h3li)
+    //   })
+    //   $('#outline__root').append(ol)
+    // }
+  })
 })
