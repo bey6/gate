3a80b83a47b3e5346dc388dd92ece03acbc2a9b5
贝 <bey.liu@outlook.com>
2020-11-24 10:41:36 +0800

    Add files via upload

---
+++ routes/plan.js
@@ -0,0 +1,49 @@
+const router = require('koa-router')()
+
+router.prefix('/plan')
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('plan/index.pug', {
+    title: '计划',
+    active: 'plan',
+    plans: [
+      {
+        key: 0,
+        desc: '支持从配置服务读取默认的映射信息',
+        level: 1,
+        class: 'list-group-item-danger',
+        done: false,
+      },
+      {
+        key: 1,
+        desc: '映射页面支持下载客户端配置文件功能',
+        level: 2,
+        class: 'list-group-item-warning',
+        done: false,
+      },
+      {
+        key: 2,
+        desc: '支持在线编辑文档、日志功能',
+        level: 3,
+        class: 'list-group-item-info',
+        done: false,
+      },
+      {
+        key: 3,
+        desc: '一般优先级',
+        level: 4,
+        class: '',
+        done: false,
+      },
+      {
+        key: 4,
+        desc: '完成的项目',
+        level: 4,
+        class: 'list-group-item-dark',
+        done: true,
+      },
+    ],
+  })
+})
+
+module.exports = router
