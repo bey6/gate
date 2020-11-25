390be4a4cd41d6c2d77462b62a32ad873509b0d7
bey <bey.liu@outlook.com>
2020-11-26 00:35:54 +0800

    feat: style opt

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,7 +1,6 @@
 $(function () {
   const mdeditor = new SimpleMDE({ element: $('#view-area')[0] })
   $('#file-list').click(e => {
-    console.log(e.target)
     if (e.target.nodeName==='PRE')
       mdeditor.value(e.target.textContent)
   })
