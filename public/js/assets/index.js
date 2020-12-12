ae26964e06f012d9d490c1c0e4f296431f3bed32
bey <bey.liu@outlook.com>
2020-12-12 17:25:10 +0800

    feat: add assets list show

---
+++ public/js/assets/index.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('#file-list').click(function (event) {
+        let t = event.target,
+            path = '',
+            type = '',
+            pref = ''
+        if (!t.dataset || !t.dataset.name) return
+
+        path = t.dataset.name ? t.dataset.name : ''
+        type = t.dataset.type
+        pref = t.dataset.prefix + '/'
+
+        if (type === 'folder') window.location = `/assets?path=${pref + path}`
+        else $().toast('copied')
+    })
+})
--- routes/assets.js
+++ routes/assets.js
@@ -1,22 +1,66 @@
 const router = require('koa-router')()
 const fs = require('fs')
+const path = require('path')
 
 router.prefix('/assets')
 
+const extMapping = {
+    '.html': 'img/HTML.png',
+    '.css': 'img/CSS.png',
+    '.js': 'img/JS.png',
+    '.json': 'img/JSON.png',
+}
+
+function isImg(ext) {
+    return ['.ico', '.png', '.jpg', '.jpeg'].includes(ext)
+}
+
 router.get('/', async (ctx, next) => {
-    let assets = []
+    let assets = [],
+        payload = ctx.query.path,
+        pref = payload ? payload[0] === '/' ? payload : '/' + payload : ''
     try {
-        const files = fs.readdirSync('public')
+        const files = fs.readdirSync('public' + pref)
         files.forEach(file => {
-            // const fileState = fs.statSync('public'+file)
-            // fileState.isDirectory()
-            console.log(file)
-            assets.push(file)
+            const fileState = fs.statSync('public' + pref + '/' + file)
+            let ext = path.extname(file)
+            let name = file, img = '', type = 'file'
+            if (fileState.isDirectory()) {
+                img = 'img/folder.png'
+                type = 'folder'
+            } else if (isImg(ext)) img = pref + '/' + file
+            else img = extMapping[ext] ?? 'img/file.png'
+
+            assets.push({
+                name,
+                img,
+                type,
+                prefix: pref
+            })
         })
+        console.log(pref)
+        let arrPath = pref.split('/')
+        console.log(arrPath)
+        arrPath = arrPath.map((p, idx) => {
+            if (idx === 0) {
+                return {
+                    path: '/assets',
+                    text: p
+                }
+            } else {
+                return {
+                    path: arrPath.slice(0, idx).join('/'),
+                    text: p
+                }
+            }
+        })
+        console.log(arrPath)
+        arrPath.shift()
         await ctx.render('assets/index.pug', {
             title: 'assets',
             active: 'assets',
-            files: assets
+            files: assets,
+            path: arrPath
         })
     } catch (error) {
         ctx.body = error
